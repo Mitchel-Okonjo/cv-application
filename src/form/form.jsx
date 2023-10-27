60fd87757cd5740223b0fbc4830c8c91d7e5cd34
Mitchel-Okonjo <111641404+Mitchel-Okonjo@users.noreply.github.com>
2023-10-27 16:11:39 +0100

    Create and style form

---
+++ src/form/form.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./form.css";
+import { PersonalDetails } from "../pages/personal";
+import { Links } from "../pages/links";
+import { Button } from "../pages/tools";
+
+const Form = ({ heading, activeIndex, onClick }) => {
+  return (
+    <section className='form-section'>
+      <div className='form-header'>
+        <h1 className='form-heading'>{heading}</h1>
+        <div className='page-btns'>
+          <Button
+            type='button'
+            className='page-navigation'
+            pageNav='Back'
+            onClick={() => onClick(activeIndex - 1)}
+            visibility={activeIndex > 0 ? "flex" : "none"}
+          />
+          <Button
+            type='button'
+            className='page-navigation'
+            pageNav='Next'
+            onClick={() => onClick(activeIndex + 1)}
+            visibility={activeIndex < 6 ? "flex" : "none"}
+          />
+        </div>
+      </div>
+      <form className='form'>
+        {activeIndex === 0 && <PersonalDetails />}
+        {activeIndex === 1 && <Links />}
+        {/* {activeIndex === 2 && <Skills />}
+        {activeIndex === 3 && <WorkExperience />}
+        {activeIndex === 4 && <Projects />}
+        {activeIndex === 5 && <Education />}
+        {activeIndex === 6 && <Certifications />} */}
+      </form>
+    </section>
+  );
+};
+
+export default Form;
